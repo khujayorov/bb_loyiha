b87838ad5bdbea0865b0e9fa5680e6bd63db49c0
khujayorov__ <xujayorovjamshidbek@gmail.com>
2025-12-16 23:18:53 +0500

    andijonda yangi o'zgarishlar kiritildi

--- viloyatlar/andijon.js
+++ viloyatlar/andijon.js
@@ -20,3 +20,25 @@ form.addEventListener("submit", function (e) {
 
   form.reset();
 });
+
+const images = document.querySelectorAll(".gallery-grid img");
+const modal = document.getElementById("imgModal");
+const modalImg = document.getElementById("modalImage");
+const closeBtn = document.querySelector(".close");
+
+images.forEach(img => {
+  img.addEventListener("click", () => {
+    modal.style.display = "flex";
+    modalImg.src = img.src;
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
